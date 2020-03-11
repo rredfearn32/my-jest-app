e003d313717d1e2922270fa35d36aca00b046a7b
robbie.redfearn <robbie.redfearn@groupgti.com>
2020-03-11 16:21:47 +0000

    Ready to start new project in branch

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+});
+
+test('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy(); 
+})
+
 test('renders Hello World header', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
+  
 });
