5ed32529b9dd1a0dcdbd8992363fc427fb52e438
Robbie Redfearn <rredfearn32@live.com>
2020-04-20 18:15:58 +0100

    Write tests for Input success=true

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import { findByTestAttr, storeFactory } from '../test/testUtils';
 import Input from './Input';
@@ -36,14 +36,22 @@ describe('render', () => {
         });
     });
     describe('word has been guessed', () => {
-        test('does not renders the component without error', () => {
-
+        let wrapper
+        beforeEach(() => {
+            const initialState = {success: true};
+            wrapper = setup(initialState);
+        })
+        test('renders the component without error', () => {
+            const component = findByTestAttr(wrapper, 'component-input');
+            expect(component.length).toBe(1);
         });
         test('does not renders the input box', () => {
-            
+            const inputBox = findByTestAttr(wrapper, 'input-box');
+            expect(inputBox.length).toBe(0);
         });
         test('does not renders the submit button', () => {
-
+            const submitButton = findByTestAttr(wrapper, 'submit-button');
+            expect(submitButton.length).toBe(0);
         });
     });
 });
