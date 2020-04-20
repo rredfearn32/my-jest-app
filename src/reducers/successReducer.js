503f01b56045de69aa47d6d06b851f32eda12d2f
Robbie Redfearn <rredfearn32@live.com>
2020-04-20 15:01:52 +0100

    Created reducer stubs, store, and wrapped the app in a provider

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @method successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - Action to be reduced.
+ * @returns {boolean} - New success sate.
+ */
+export default (state, action) => {
+    return null;
+}
