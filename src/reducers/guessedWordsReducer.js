8282e0471e54dc501ba2d4b820913b03b87fc93f
Robbie Redfearn <rredfearn32@live.com>
2020-04-22 02:29:02 +0100

    Set up guessedWords test

---
+++ src/reducers/guessedWordsReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function gussedWordsReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} state - Action to be reduced.
+ * @returns {array} - new guessedWords state.
+ */
+export default (action, state) => {
+    return null;
+}
