a9cde3d274c34e4e9ef01ac2b2649256aedb707d
Robbie Redfearn <rredfearn32@live.com>
2020-04-23 14:32:51 +0100

    Make tests and code for guessWord arg

--- src/Input.js
+++ src/Input.js
@@ -4,13 +4,47 @@ import { connect } from 'react-redux';
 import { guessWord } from './actions'; 
 
 export class UnconnectedInput extends Component {
+    /**
+     * @method constructor
+     * @param {object} props - Component props
+     * @returns {undefined}
+     */
+    constructor(props) {
+        super(props);
+
+        // initialize state
+        this.state = { currentGuess: null };
+
+        // bind this for submitGuessedWord
+        this.submitGuessedWord = this.submitGuessedWord.bind(this);
+    }
+
+    submitGuessedWord(ev) {
+        ev.preventDefault();
+        const guessedWord = this.state.currentGuess;
+
+        if(guessedWord && guessedWord.length > 0) {
+            this.props.guessWord(guessedWord);
+        }
+    }
+    
     render() {
         const contents = this.props.success
         ? null
         : (
           <form className="form-inline">
-              <input data-test="input-box" className="mb-2 mx-sm-3" type="text" placeholder="Enter guess..." />
-              <button data-test="submit-button" type="submit" className="btn btn-primary" onClick={() => this.props.guessWord('train')}>
+              <input
+                data-test="input-box"
+                className="mb-2 mx-sm-3"
+                type="text"
+                placeholder="Enter guess..."
+                value={this.state.currentGuess}
+                onChange={(ev) => this.setState({ currentGuess: ev.target.value })} />
+              <button
+                data-test="submit-button"
+                type="submit"
+                className="btn btn-primary"
+                onClick={(ev) => this.submitGuessedWord(ev)}>
                   Submit
               </button>
           </form>  
--- src/Input.test.js
+++ src/Input.test.js
@@ -71,25 +71,35 @@ describe('redux props', () => {
 });
 
 describe('"guessWord" action creator', () => {
-    test('calls "guessWord" when button is clicked', () => {
-        const guessWordMock = jest.fn();
-    
+    let guessWordMock;
+    let wrapper;
+    let guessedWord;
+
+    beforeEach(() => {
+        // Create a mock function for getSecretWord
+        guessWordMock = jest.fn();
         const props = {
             guessWord: guessWordMock,
             success: false
         }
-    
-        // Set up Input component with guessWordMock as the guessWord prop
-        const wrapper = shallow(<UnconnectedInput {...props} />);
-    
-        // Get the submit button
-        const submitButton = findByTestAttr(wrapper, 'submit-button');
-        
-        // Simulte clicking the button
-        submitButton.simulate('click');
+        guessedWord = 'train';
 
+        // Set up Input, with guessWordMock as a prop
+        wrapper = shallow(<UnconnectedInput {...props} />);
+
+        // add value to input box
+        wrapper.setState({ currentGuess: guessedWord });
+
+        const submitButton = findByTestAttr(wrapper, 'submit-button');
+        submitButton.simulate('click', { preventDefault(){} });
+    });
+
+    test('calls "guessWord" when button is clicked', () => {
         const guessWordCallCount = guessWordMock.mock.calls.length;
-    
         expect(guessWordCallCount).toBe(1);
+    });
+    test('calls "guessWord" with input value as argument', () => {
+        const guessWordArg = guessWordMock.mock.calls[0][0];
+        expect(guessWordArg).toBe(guessedWord);
     });
 });
