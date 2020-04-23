3adcc4c64c2fdc8a5c6dd55e8bbf1d3a94fcef0d
Robbie Redfearn <rredfearn32@live.com>
2020-04-23 11:35:35 +0100

    Add redux prop tests to Input

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { guessWord } from './actions'; 
+
 class Input extends Component {
     render() {
         const contents = this.props.success
@@ -26,4 +28,4 @@ const mapStateToProps = ({ success }) => {
     return { success };
 }
 
-export default connect(mapStateToProps)(Input);
+export default connect(mapStateToProps, { guessWord })(Input);
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+
 import { getLetterMatchCount } from '../helpers';
 
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
-    GUESS_WORD: 'GUESS_WORD'
+    GUESS_WORD: 'GUESS_WORD',
+    SET_SECRET_WORD: 'SET_SECRET_WORD'
 };
 
 /**
@@ -29,3 +32,19 @@ export const guessWord = (guessedWord) => {
         }
     };
 };
+
+/**
+ * @function getSecretWord
+ * @returns {function} - Redux Thunk function.
+ */
+export const getSecretWord = () => {
+    return (dispatch) => {
+        return axios.get('http://localhost:3030')
+            .then(response => {
+                dispatch({
+                    type: actionTypes.SET_SECRET_WORD,
+                    payload: response.data
+                });
+            });
+    };
+}
