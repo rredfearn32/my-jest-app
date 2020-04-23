601dd319dee7e4157102c841957d8926b3bff587
Robbie Redfearn <rredfearn32@live.com>
2020-04-23 12:49:57 +0100

    Connected App component (and tests)

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,49 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
+import { storeFactory } from '../test/testUtils';
 import App from './App';
 
-Enzyme.configure({
-  adapter: new EnzymeAdapter()
-});
+/**
+ * @function setup
+ * @param {object} initialState - State for this setup.
+ * @returns {ShallowWrapper}
+ */
+const setup = (initialState={}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<App store={store} />).dive().dive();
+  return wrapper;
+};
 
 test('renders without crashing', () => {
-  const wrapper = shallow(<App />);
+  const wrapper = setup();
   expect(wrapper).toBeTruthy(); 
+});
+
+describe('redux props', () => {
+  test('has access to the "success" state', () => {
+    const success = false;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test('has access to the "secretWord" state', () => {
+    const secretWord = 'party';
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  test('has "guessedWords" as a piece of state', () => {
+    const guessedWords = [
+      {guessedWord: 'train', letterMatchCount: 3 },
+    ];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toBe(guessedWords);
+  });
+  test('"getSecretWord" action creator is a function on the props', () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
 });
