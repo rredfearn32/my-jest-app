30267045520dbb997261f9e6239a7e0883ae63a0
Robbie Redfearn <rredfearn32@live.com>
2020-04-28 16:23:49 +0100

    Added tests for submission and state update on Input form

--- src/Input.js
+++ src/Input.js
@@ -2,8 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ({ secretWord }) => {
+    // useState is not destructured so that we can mock it later
+    const [ currentGuess, setCurrentGuess ] = React.useState('');
+    const submitGuess = (ev) => {
+        ev.preventDefault();
+        console.log('Foo bar');
+        setCurrentGuess('');
+    };
+
     return (
-    <div data-test="component-input">{ secretWord }</div>
+    <div data-test="component-input">
+        <form 
+            className="form-inline"
+            onSubmit={(ev) => submitGuess(ev)}>
+            <input
+                data-test="input-box"
+                className="mb-2 mr-sm-3"
+                type="text"
+                placeholder="Enter guess"
+                value={currentGuess}
+                onChange={(ev) => setCurrentGuess(ev.target.value)} />
+            <button
+                data-test="submit-button"
+                className="btn btn-primary mb-2"
+                onClick={(ev) => submitGuess(ev)}>
+                Submit
+            </button>
+        </form>
+    </div>
     );
 };
 
--- src/Input.test.js
+++ src/Input.test.js
@@ -22,3 +22,29 @@ test('Input renders without error', () => {
 test('Input has a secretWord', () => {
     checkProps(Input, { secretWord: 'party' });
 });
+
+describe('state controlled input field', () => {
+    let mockSetCurrentGuess = jest.fn(), wrapper;
+    beforeEach(() => {
+        mockSetCurrentGuess.mockClear();
+        React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+        
+        wrapper = setup();
+    });
+    test('currentGuess state updates upon input box value change', () => {
+        const inputBox = findByTestAttr(wrapper, 'input-box');
+
+        const mockEvent = { target: { value: 'train' } };
+        inputBox.simulate('change', mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+    });
+    test('currentGuess state gets cleared upon submit button click', () => {
+        const submitButton = findByTestAttr(wrapper, 'submit-button');
+        
+        const mockEvent = { preventDefault: () => {} }
+        submitButton.simulate('click', mockEvent);
+
+        expect(mockSetCurrentGuess).toHaveBeenCalled();
+    });
+});
