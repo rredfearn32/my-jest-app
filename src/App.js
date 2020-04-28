76c01cdc26292c86811022478c9aa528dffb1fa6
Robbie Redfearn <rredfearn32@live.com>
2020-04-28 14:32:30 +0100

    Basic Input component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-import GuessedWords from './GuessedWords';
-import Congrats from './Congrats';
-
-class App extends Component {
-  render() {
-    return (
-      <div data-test="component-app">
-        
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div data-test="component-app"></div>
+  );
+};
 
 export default App;
