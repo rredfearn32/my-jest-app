979a8600af5674ed42c5f2172d8a2bdfe6d7b2d1
Robbie Redfearn <rredfearn32@live.com>
2020-05-01 14:49:46 +0100

    Integrated successContext into Input + tests

--- src/Input.js
+++ src/Input.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import successContext from './contexts/successContext';
 import languageContext from './contexts/languageContext';
 import stringsModule from './helpers/strings';
 
 const Input = ({ secretWord }) => {
     const language = React.useContext(languageContext);
+    const [ success, setSuccess ] = successContext.useSuccess();
     const [ currentGuess, setCurrentGuess ] = React.useState('');
     const submitGuess = (ev) => {
         ev.preventDefault();
@@ -13,25 +15,29 @@ const Input = ({ secretWord }) => {
     };
 
     return (
-    <div data-test="component-input">
-        <form 
-            className="form-inline"
-            onSubmit={(ev) => submitGuess(ev)}>
-            <input
-                data-test="input-box"
-                className="mb-2 mr-sm-3"
-                type="text"
-                placeholder={stringsModule.getStringByLanguage(language, 'guessInputPlaceholder')}
-                value={currentGuess}
-                onChange={(ev) => setCurrentGuess(ev.target.value)} />
-            <button
-                data-test="submit-button"
-                className="btn btn-primary mb-2"
-                onClick={(ev) => submitGuess(ev)}>
-                {stringsModule.getStringByLanguage(language, 'submit')}
-            </button>
-        </form>
-    </div>
+        success 
+            ?
+        null
+            :
+        <div data-test="component-input">
+            <form 
+                className="form-inline"
+                onSubmit={(ev) => submitGuess(ev)}>
+                <input
+                    data-test="input-box"
+                    className="mb-2 mr-sm-3"
+                    type="text"
+                    placeholder={stringsModule.getStringByLanguage(language, 'guessInputPlaceholder')}
+                    value={currentGuess}
+                    onChange={(ev) => setCurrentGuess(ev.target.value)} />
+                <button
+                    data-test="submit-button"
+                    className="btn btn-primary mb-2"
+                    onClick={(ev) => submitGuess(ev)}>
+                    {stringsModule.getStringByLanguage(language, 'submit')}
+                </button>
+            </form>
+        </div>
     );
 };
 
--- src/Input.test.js
+++ src/Input.test.js
@@ -4,19 +4,23 @@ import { findByTestAttr, checkProps } from '../test/testUtils';
 
 import Input from './Input';
 import languageContext from './contexts/languageContext';
+import successContext from './contexts/successContext';
 
 /**
  * Setup function for Input component
  * @param {String} testValues
  * @returns {ReactWrapper} 
  */
-const setup = ({ language, secretWord }) => {
+const setup = ({ language, secretWord, success }) => {
     language = language || 'en';
     secretWord = secretWord || 'party';
+    success = success || false;
 
     return mount(
         <languageContext.Provider value={language}>
-            <Input secretWord={secretWord} />
+            <successContext.SuccessProvider value={[success, jest.fn()]}>
+                <Input secretWord={secretWord} />
+            </successContext.SuccessProvider>
         </languageContext.Provider>
     );
 };
@@ -68,4 +72,9 @@ describe('languagePicker', () => {
         const submitButton = findByTestAttr(wrapper, 'submit-button');
         expect(submitButton.text()).toBe('Einreichen');
     });
-})
+});
+
+test('input component does not show when success is true', () => {
+    const wrapper = setup({ secretWord: 'party', success: true });
+    expect(wrapper.isEmptyRender()).toBe(true);
+});
